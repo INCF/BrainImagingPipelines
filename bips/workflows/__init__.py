c5756025aac280bd33e64415427c88267943a771
Anisha Keshavan <akeshavan@ucla.edu>
2012-04-25 19:40:09 -0400

    wip: workflow6 added and added 9 to init

--- bips/workflows/__init__.py
+++ bips/workflows/__init__.py
@@ -3,10 +3,10 @@ import workflow2
 import workflow3
 import workflow4
 import workflow5
-#import workflow6
+import workflow6
 #import workflow7
 import workflow8
-#import workflow9
+import workflow9
 import workflow10
 
 from .base import (list_workflows, configure_workflow, run_workflow,
