5f93f2401f33c394ddbf804d3741c461a4e0c2fe
Satrajit Ghosh <satra@mit.edu>
2012-06-19 01:15:42 -0400

    enh: display workflow config info

--- bips/service/base.py
+++ bips/service/base.py
@@ -116,64 +116,30 @@ class BIPS(object):
     def info(self, uuid):
         wf = get_workflow(uuid)
         val = wf.get()
-        json_str =  json.dumps(val, cls=MyEncoder)
-        config_str = json.dumps(wf.config_ui().get(), cls=MyEncoder)
+        json_str =  val #json.dumps(val, cls=MyEncoder)
+        config_str = wf.config_ui().get() #json.dumps(wf.config_ui().get(), cls=MyEncoder)
         img_file = ''
-        msg = """
-<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-  <title> BIPS: Workflow info</title>
-  <style type='text/css'>
-    pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
-.string { color: green; }
-.number { color: darkorange; }
-.boolean { color: blue; }
-.null { color: magenta; }
-.key { color: red; }
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        return json.dumps({'jsonconfig': json_str, 'workflowconfig': config_str},
+                          cls=MyEncoder)
+    '''
+            msg = """
 
-  </style>
-</head>
-<body>
-
-<script type="text/javascript">
-function syntaxHighlight(json) {
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
-var str = JSON.stringify(%s, null, 2);
-document.write('<h3>Workflow info</h3>')
-document.write('<pre>' + syntaxHighlight(str) +'</pre>');
-var str2 = JSON.stringify(%s, null, 2);
-document.write('<h3>Workflow config</h3>')
-document.write('<pre>' + syntaxHighlight(str2) +'</pre>');
-document.write('<h3>Workflow graph</h3>')
-document.write('<img src="%s" />')
-</script>
-
-</body>
-
-
-</html>
-""" % (json_str, config_str, img_file)
-        return msg
-
+    var str = JSON.stringify(%s, null, 2);
+    $(this).append('<h3>Workflow info</h3>');
+    console.log($(this));
+    /*
+    document.write('<pre>' + syntaxHighlight(str) +'</pre>');
+    var str2 = JSON.stringify(%s, null, 2);
+    document.write('<h3>Workflow config</h3>')
+    document.write('<pre>' + syntaxHighlight(str2) +'</pre>');
+    document.write('<h3>Workflow graph</h3>')
+    document.write('<img src="%s" />')
+    */
+    </script>
+    """ % (json_str, config_str, img_file)
+            return msg
+    '''
     @expose
     def configure(self, uuid):
         wf = get_workflow(uuid)
