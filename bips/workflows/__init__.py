962015c5f2d453e825510f31edff64b19b4640ed
Anisha Keshavan <akeshavan@ucla.edu>
2012-09-21 19:19:49 -0400

    doc: added new logo

--- bips/workflows/__init__.py
+++ bips/workflows/__init__.py
@@ -1,6 +1,13 @@
-from .base import (list_workflows, configure_workflow, run_workflow,
-                    display_workflow_info)
-=======
+import workflow2
+import workflow3
+import workflow4
+import workflow5
+import workflow6
+import workflow7
+import workflow8
+import workflow9
+import workflow10
+import workflow11
 import workflow12
 import workflow13
 import synced_corr_display_h5
