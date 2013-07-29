8436599214b322f991da21b156713255f4603562
Doreen Reuchsel <doreenr@ba3.mit.edu>
2013-07-29 08:07:09 -0400

    Workflow not quite registered, move stimulus_motion to fmri/first_level

--- bips/workflows/__init__.py
+++ bips/workflows/__init__.py
@@ -1,13 +1,14 @@
 from .base import (get_workflow, get_workflows, list_workflows,
                    configure_workflow, run_workflow, display_workflow_info)
 from .gablab.wips.dicom import dicom_conversion
-from .gablab.wips.fmri.first_level import first_level_QA, first_level, fixed_effects, first_level_ev, spm_first_level
+from .gablab.wips.fmri.first_level import first_level_QA, first_level, fixed_effects, first_level_ev, spm_first_level, stim_mot
 from .gablab.wips.fmri.group_analysis import fsl_one_sample_t_test, fsl_multiple_regression, one_sample_t_surface, spm_group_analysis
 from .gablab.wips.fmri.misc import compare_realignment_nodes, seg_stats_individual, better_surface_localizer, group_segstats, flirt_reg
 from .gablab.wips.fmri.preprocessing import fmri_preprocessing, fmri_QA, spm_preprocessing, preproc_QA_json, \
                                             FIR_filter, preproc_no_freesurfer, group_preproc_QA, preproc_QA_json, \
                                             fmri_extras, simple_resting
 from .gablab.wips.fmri.resting import wip_resting_correlation_QA, map_correlations, seed_based_connectivity, seed_based_connectivity2
+from .gablab.wips.fmri.stimulus_motion import stim_mot
 from .gablab.wips.fmri.viz import synced_corr_display_h5
 from .gablab.wips.smri import test_freesurfer, normalize_structural, normalize_functionals, kelly_kapowski, freesurfer_brain_masks, wip_divide_parcellations
 from .gablab.wips.utils import take_mean_image, plot_contours, change_datatype
